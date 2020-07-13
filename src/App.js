e6a3aeeb47c6ebe8edc9e33b2ca319730af0f9ab
kevinadhiguna <kevin26798@gmail.com>
2020-07-13 16:45:08 +0700

    refactor: version0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import './App.css';
+import axios from 'axios';
+import PokemonList from './PokemonList';
+import Pagination from './Pagination';
+import { ClipLoader } from 'react-spinners';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios.get(currentPageUrl, {
+        cancelToken: new axios.CancelToken(c => cancel = c)
+      })
+      .then(res => {
+        setLoading(false);
+        setPrevPageUrl(res.data.previous);
+        setNextPageUrl(res.data.next);
+        setPokemon(res.data.results.map(p => p.name));
+      })
+
+    return () => cancel();
+
+  }, [currentPageUrl]);
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  if (loading) return (
+    <div>
+      <p>loading...</p>
+      <ClipLoader />
+    </div>
+  );
+
+  return ( 
+    <div className = "App" >
+      <PokemonList pokemon = {pokemon}
+      /> 
+      <Pagination 
+        goToPrevPage = {prevPageUrl ? goToPrevPage : null}
+        goToNextPage = {nextPageUrl ? goToNextPage : null}
+      />
     </div>
   );
 }
---
+++ src/Pagination.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Pagination({ goToPrevPage, goToNextPage }) {
+    return (
+        <div>
+            {goToPrevPage &&
+                <button onClick={goToPrevPage}>Previous</button>
+            }
+            {goToNextPage &&
+                <button onClick={goToNextPage}>Next</button>
+            }
+        </div>
+    );
+}
